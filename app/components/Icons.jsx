a2afb2315cceb63833668992c1925f62cd4f023f
Radim Zavadil <rada07zavadil@gmail.com>
2025-06-21 23:33:32 +0200

    Tweak 404 page and whole website layout

--- app/components/Icons.jsx
+++ app/components/Icons.jsx
@@ -9,11 +9,11 @@ import linkedlnIcon from './linkedln-icon.png';
 
 export default function Icons() {
   return (
-    <main className='flex items-center text-gray-400 m-2'>
-        <Link href=""><Image src={twitterIcon} alt='X logo' quality={100} className='icon'/></Link>
-        <Link href=""><Image src={instagramIcon} alt='Instagram logo' quality={100} className='icon'/></Link>
-        <Link href=""><Image src={githubIcon} alt='Github logo' quality={100} className='icon'/></Link>
-        <Link href=""><Image src={linkedlnIcon} alt='Linkedln logo' quality={100} className='icon'/></Link>
+    <main className='flex items-center text-gray-400 mb-3'>
+        <Link href="https://x.com/radimzavadil16" target='_blank'><Image src={twitterIcon} alt='X logo' quality={100} className='m-1 w-[22px] hover:opacity-50 hover:duration-100 hover:ease-in'/></Link>
+        <Link href="https://instagram.com/radimzavadil_" target='_blank'><Image src={instagramIcon} alt='Instagram logo' quality={100} className='m-1 w-6 hover:opacity-50 hover:duration-100 hover:ease-in'/></Link>
+        <Link href="https://github.com/Radim-Zavadil" target='_blank'><Image src={githubIcon} alt='Github logo' quality={100} className='m-1 w-6 hover:opacity-50 hover:duration-100 hover:ease-in'/></Link>
+        <Link href="https://www.linkedin.com/in/radim-zavadil-400043371/" target='_blank'><Image src={linkedlnIcon} alt='Linkedln logo' quality={100} className='m-1 w-6 hover:opacity-50 hover:duration-100 hover:ease-in'/></Link>
     </main>
   )
 }
